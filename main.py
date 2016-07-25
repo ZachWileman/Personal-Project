d385618b10ad65317617c1f257f3db005cbf771a
Zachary Wileman <zdw27f@mst.edu>
2016-07-25 11:44:01 -0500

    Changes:
    -Added a check to run the program every five minutes
    -Integrated the Channel class
    -Shortened the amount of lines
    -Added some more comments

--- main.py
+++ main.py
@@ -10,11 +10,13 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import sys, os, re
+import sys, os, re, time
 
+# Class used for saving info for each YouTube channel
 class Channel:
-    def __init__(self):
+    def __init__(self, channelName):
         self.latestVideo = ""
+        self.name = channelName
     def checkVideo(self, videoTitle):
         if videoTitle != self.latestVideo:
             print('New video found!')
@@ -25,39 +27,61 @@ class Channel:
 
 YOUTUBE_LINK = 'https://www.youtube.com/'
 
+# Gets user input for channels
 channels = input('Please enter YouTube channels you\'d like to follow, each ' +
                  'separated by a comma.\nEx.(channel1, channel2, ect.): ').split(', ')
-print(channels)
-htmlFiles = []
 
-# This is the portion I want to repat every five minutes; checking for new videos
-# Also, look into saving latest video information to some sort of log file, maybe json?
-for number, channel in enumerate(channels):
-    try:
-        htmlFiles.append(urlopen(YOUTUBE_LINK + channel + '/Videos'))
-    except:
-        print('An error occurred while attempting to open a YouTube link.')
-        sys.exit(1)
+# Creates a list of Channel objects
+channelList = [Channel(channel) for channel in channels]
 
-    htmlFiles[number] = str(htmlFiles[number].read())
+############### Also, look into saving latest video information to some sort of log file, maybe json?
 
-    soup = BeautifulSoup(htmlFiles[number], 'html.parser')
-    htmlFiles[number] = soup.prettify()
-    regExp = re.compile('<h3\sclass=\"yt-lockup-title\s\">\n.*href=\"/(.*)\"\stitle=\"(.*)\">')
-    match = regExp.search(str(htmlFiles[0]))
-    if match:
+# Loops every five minutes, checking for new videos from the given channels
+while True:
+    for channel in channelList:
+        try:
+            htmlFile = urlopen(YOUTUBE_LINK + channel.name + '/Videos')
+        except:
+            print('An error occurred while attempting to open a YouTube link.')
+            sys.exit(1)
 
-        print('New video found: ', match.group(2))
-        downloadString = 'youtube-dl -o "E:/Users/spart/Videos/YouTube Videos/%(title)s.%(ext)s" ' + YOUTUBE_LINK + match.group(1)
-        print(downloadString)
-        os.system(downloadString)
-    else:
-        print('Unable to find a match within the HTML file.')
+        # Cleans up the html file
+        htmlFile = str(htmlFile.read())
+        soup = BeautifulSoup(htmlFile, 'html.parser')
+        htmlFile = soup.prettify()
 
-    # Don't need these lines when done. Temporarily using for viewing files.
-    fileName = 'htmlFile' + str(number) + '.html'
-    with open(fileName, 'w') as f:
-        f.write(htmlFiles[number])
+        # Search through the html file for a match with the regular expression
+        regExp = re.compile('<h3\sclass=\"yt-lockup-title\s\">\n.*href=\"/(.*)\"\stitle=\"(.*)\">')
+        match = regExp.search(htmlFile)
+        urlLink = match.group(1)
+        videoFound = match.group(2)
+
+        # If a match was successfully found
+        if match:
+
+            # Checks if a new video has been found. If so, reports so and
+            # downloads the video
+            if channel.latestVideo != videoFound:
+                print('New video found: ', videoFound)
+                downloadString = 'youtube-dl -o "E:/Users/spart/Videos/YouTube Videos/%(title)s.%(ext)s" ' + YOUTUBE_LINK + urlLink
+                channel.latestVideo = videoFound
+                os.system(downloadString)
+            
+            # Otherwise, reports nothing new found.
+            else:
+                print('Nothing new found.')
+
+        # If unable to find a regular expression match, reports so.
+        else:
+            print('Unable to find a match within the HTML file.')
+
+    # Has the program check every 5 minutes for new videos
+    time.sleep(5*60)
+
+        # Don't need these lines when done. Temporarily using for viewing files.
+        #fileName = 'htmlFile' + str(number) + '.html'
+        #with open(fileName, 'w') as f:
+        #    f.write(htmlFiles[number])
 
 # When using regex to find links in the html, use: 'h3 class="yt-lockup-title'
 # <h3 class="yt-lockup-title ">\n.*
