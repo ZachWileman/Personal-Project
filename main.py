4195854d6b5a6f70b6c779296d72d237f9c464a2
Zachary Wileman <zdw27f@mst.edu>
2017-02-19 06:55:18 -0600

    Changes:
    -Fixed some issues with JSON with storing latestVideo info
    -Added the ability for users to receive email notifications when a
     new video is downloaded

--- main.py
+++ main.py
@@ -7,11 +7,20 @@
 
 # NOTE: I'm using the github project 'youtube-dl' to download the YouTube
 # videos. I do not endorse the downloading of YouTube videos. The program
-# I have made was made purley for educational purposes.
+# I have made was made purely for educational purposes.
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import sys, os, re, time, json
+from smtplib import SMTP
+
+# Global values
+YOUTUBE_LINK = 'https://www.youtube.com/'
+userInfo = {'toEmailAdd': None, 'fromEmailAdd': None, 'password': None}
+
+# Used for when importing JSON file
+noFileFound = False 
+noUserInfoFound = False
 
 # Class used for saving info for each YouTube channel
 class Channel:
@@ -30,19 +39,15 @@ class Channel:
         else:
             return False 
 
-YOUTUBE_LINK = 'https://www.youtube.com/'
+
 
 # Gets user input for channels
 # NOTE: With newer channels, usernames no longer work so instead you'll have
 # to enter part of the url of the channel such as: 'channel/' followed by the
 # channel id
 channels = input('Please enter YouTube channels you\'d like to follow, each ' +
-                 'separated by a comma.\nEx.(channel1, channel2, ect.): ').split(', ')
-
-############################# Also, possibly add something for entering new channels while the
-############################# program is running?
-
-############################# Also, add option for inputting download location
+                 'separated by a comma.\nEx.(channel1, channel2, ect.): \n').split(', ')
+print('') # Adds new line
 
 # Checks for empty list
 if '' not in channels:
@@ -52,34 +57,81 @@ else:
     channelList = []
 
 # Gets user choice for importing previous channel info
-importChoice = input('Would you like to import past channel info from' +
-                     ' previous program runs? (yes or no): ')
+importChoice = input('Would you like to import past channel/user info from previous program runs? (yes or no): \n' +
+                     'WARNING: Selecting no will cause old information to be overwritten by new information if any is entered\n')
+print('') # Adds new line
 
 # If the user chooses to import
-if importChoice == 'yes':
+if importChoice == 'yes' or importChoice == 'y':
     
     # Tries to import the JSON file 
     try:
 
         # Loads in the JSON file and adds the info to channelList if the
         # channel isn't already in the list.
-        with open('youtubeData.json', 'r') as f:
+        with open('data.json', 'r') as f:
             jsonList = json.load(f)
-        for channel in jsonList:
-            temp = Channel(channel['channelName'])
-            temp.latestVideo = channel['latestVideo']
-            if channel['channelName'] not in channels:
-                channelList.append(temp)
+
+        # Checks in case the JSON file contains an empty dictionary
+        if jsonList:
+            
+            # Checks for channel info
+            if 'channels' in jsonList:
+                for channel in jsonList['channels']:
+                    temp = Channel(channel['channelName'])
+                    temp.latestVideo = channel['latestVideo']
+                    if channel['channelName'] not in channels:
+                        channelList.append(temp)
+
+            # Checks for user email info
+            if 'user' in jsonList:
+                userInfo['fromEmailAdd'] = jsonList['user']['fromEmailAdd']
+                userInfo['toEmailAdd'] = jsonList['user']['toEmailAdd']
+                userInfo['password'] = jsonList['user']['password']
+            else:
+                noUserInfoFound = True
+        else:
+            print('No data found in file to import\n')
     
     # If there is no JSON file to import, reports so and then continues
     except FileNotFoundError as e:
-        print('No file found to import data from.')
+        print('No file found to import data from\n')
+        noFileFound = True
 
+# If no data has been imported
+if noFileFound or noUserInfoFound or (importChoice != 'yes' and importChoice != 'y'):
 
-# Make a json list out of channelList to then put in a json file
-jsonList = [{"channelName": channel.name, "latestVideo": channel.latestVideo} for channel in channelList]
-with open('youtubeData.json', 'w') as f:
-    json.dump(jsonList, f)
+    # Gets email from user for sending notifications
+    userInfo['fromEmailAdd'] = input('Please enter the email you would like to send emails from: ' +
+                                   '(Leave blank if you don\'t want notifications)\n')
+    print('') # Adds new line
+
+    # Gets passsword (for sending emails) if email is entered
+    if userInfo['fromEmailAdd']:
+        userInfo['password'] = input('Please enter the password for your (sending) email address: ' +
+                                     '(Your password and email will only be stored in a local JSON file)\n')
+        print('') # Adds new line
+
+        # Gets email from user for recieving notifications
+        userInfo['toEmailAdd'] = input('Please enter the email you would like to receive notifications at: ' +
+                                       '(Leave blank if you don\'t want notifications)\n')
+        print('') # Adds new line
+
+        with open('data.json', 'w') as f:
+            json.dump({"user": {"toEmailAdd": userInfo['toEmailAdd'], "fromEmailAdd": userInfo['fromEmailAdd'], "password": userInfo['password']}}, f, indent=4)
+
+# In case a file has/hasn't been created
+try:
+    # Opens JSON file and adds new channel info
+    with open('data.json', 'r') as f:
+        newData = json.load(f)
+        newData['channels'] = [{"channelName": channel.name, "latestVideo": channel.latestVideo} for channel in channelList]
+except:
+    newData = {}
+
+# Writes the new JSON data to the file
+with open('data.json', 'w') as f:
+    json.dump(newData, f, indent=4)
 
 if not channelList:
     print('No channels were entered or imported from the JSON file.\nExiting program.')
@@ -115,6 +167,29 @@ while True:
                 downloadString = 'youtube-dl -o "E:/Users/spart/Videos/YouTube Videos/%(title)s.%(ext)s" ' + YOUTUBE_LINK + urlLink
                 channel.latestVideo = videoFound
                 os.system(downloadString)
+
+                # Sends the user an email notification about the new video downloaded
+                # (Assuming the user added an email)
+                if userInfo['fromEmailAdd']:
+                    fromAdd = username = userInfo['fromEmailAdd']
+                    toAdd = userInfo['toEmailAdd']
+                    password = userInfo['password']
+                    msg = ('Subject: New YouTube Video!\r\n' +
+                           'You\'ve got a new video downloaded from {} titled:\n{}'.format(channel.name, channel.latestVideo))
+                    
+                    server = SMTP('smtp.gmail.com:587')
+                    server.ehlo()
+                    server.starttls()
+
+                    # Attempts to login and send email
+                    try:
+                        server.login(username, password)
+                        server.sendmail(fromAdd, toAdd, msg)
+                    except Exception as e:
+                        print("You have entered an invalid email or password")
+                        print(e)
+
+                    server.quit()
             
             # Otherwise, reports nothing new found.
             else:
@@ -124,5 +199,18 @@ while True:
         else:
             print('Unable to find a match within the HTML file.')
 
+    # Updates JSON file with new 'lastest video' info for each channel
+    with open('data.json', 'r') as f:
+        newData = json.load(f)
+        newData['channels'] = [{"channelName": channel.name, "latestVideo": channel.latestVideo} for channel in channelList]
+
+    with open('data.json', 'w') as f:
+        json.dump(newData, f, indent=4)
+
     # Has the program check every 5 minutes for new videos
+    print('Waiting for 5 minutes to check for any new videos...\n')
     time.sleep(5*60)
+
+
+############################# Also, possibly add something for entering new channels while the
+############################# program is running?
